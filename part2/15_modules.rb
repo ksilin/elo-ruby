1fb75f5b6cd62483df6dd025719132387f1e7d9b
kostja <konstantin@silin.info>
2012-08-21 16:47:48 +0200

    added webworkers termination

--- part2/15_modules.rb
+++ part2/15_modules.rb
@@ -7,7 +7,7 @@
 module Render
 
   # lexical scoping - the class is declared below the constant and is not visible yet
-  # DEFAULT_FONT = Font.new("default") would not work
+  # DEFAULT_FONT = Font.new("default") would not work here
 
   class Font
     attr_accessor :font
@@ -16,10 +16,18 @@ module Render
       @font = font
     end
 
+    DEFAULT_FONT = Font.new("default")
+
     #when defining utility methods, go with module level methods, not regular methods
-         # important - use 'self' !
+    # important - use 'self' !
+    # these methods can be accessed liek classes over the namespace: Render::points_to_inches
+    # but mostly, programmers prefer to call them with hte dot: Render.mopoints_to_inches
     def self.points_to_inches(points)
       points/72.0
+    end
+
+    def self.inches_to_points(inches)
+      inches*72.0
     end
   end
 
@@ -28,7 +36,8 @@ module Render
 end
 
 # modules define a namespace, nested modules define nested namespaces
-p Render::Font.new("bla").font
+p "Render::Font.new('bla').font: #{Render::Font.new("bla").font}"
+p "default font : #{Render::DEFAULT_FONT}"
 
 # modules can be used as objects, e.g. to switch behaviour at runtime
 # here we could be using different modules defining different fonts
