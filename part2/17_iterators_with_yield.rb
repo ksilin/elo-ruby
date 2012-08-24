3df83406545cf99fc77f3bfb0f458e8f48f03a11
kostja <konstantin@silin.info>
2012-08-24 14:38:12 +0200

    finished iter with yield examples

--- part2/17_iterators_with_yield.rb
+++ part2/17_iterators_with_yield.rb
@@ -1,4 +1,7 @@
 class Doc
+  # including enumerable and implementing each yields methods like "to_a", "find", "find_all"
+  # if elements yielded by each implement "<=>" (which strings do), Enumerable also gives us "sort" and more
+  include Enumerable
 
   attr_accessor :content
 
@@ -16,6 +19,7 @@ class Doc
 
   # note that the iterator methods can be created to simulate collections that are not there
   # here - to iterate over word pairs
+  # implementing the each method allows you to include Enumerable module to do all kinds of funky stuff
   def each_word_pair
 
     words = @content.split
@@ -24,6 +28,29 @@ class Doc
     end
   end
 
+  # including enumerable lets us use "each_cons"
+  # it works like each, but applying the block to an array of consecutive elements of the specified size
+  # the array "slides" along the each method, one element at a time
+  # "each_split" does the same, but uses the size of the array as the stride  -
+  # so effectively, "each_cons" is an "each_split" with a stride of 1
+  def each_word_pair_cons
+      # we operate on 2-word arrays now
+      @content.split.each_cons(2) {|array| yield(array[0], array[1]) }
+  end
+
+  #in case we are sensitive to exceptions - for example if we need to close some resources at the end of the iteration:
+  def yield_with_exception_handling
+
+    p "opening expensive resource"
+
+    begin
+      p "yielding"
+    yield "resource"
+    # if yielding to a block will throw, the code inside ensure will run before throwing
+    ensure
+      p "closing expesive resource"
+    end
+  end
 end
 
 d = Doc.new
@@ -35,3 +62,21 @@ end
 d.each_word_pair do |w, x|
   p "pair : #{w}, #{x}"
 end
+
+d.each_word_pair_cons do |w, x|
+  p "pair : #{w}, #{x}"
+end
+
+# in case you have multiple iterating each-like methods, ruby provides the "Enumerator",
+# so you can use a different iterating method with the gains of including Enumerable
+enum = Enumerator.new(d, :each_word_pair_cons)
+
+p enum.sort
+
+# less desctructive alternative sto raise would be "break" or explicit "return"
+d.yield_with_exception_handling do
+  raise "kaboom!"
+end
+
+
+
